13eb79be7ee6b07bbecd47d18a5446a690f15324
fumiko <fumi811bbc@gmail.com>
2015-06-27 19:26:32 +0900

    Initial Commit

---
+++ NARAIGOTO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NARAIGOTO
+//
+//  Created by 山本文子 on 2015/06/27.
+//  Copyright (c) 2015年 山本文子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
